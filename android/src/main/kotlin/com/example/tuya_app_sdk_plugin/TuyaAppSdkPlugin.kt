49f9fa3019d414fa27a2586b84bd79cc696c4925
Pattarapon Tiewghan <pattarapon@code-app.com>
2023-09-13 14:09:07 +0700

    [RJ3-3232] implemented tuya sdk on android

--- android/src/main/kotlin/com/example/tuya_app_sdk_plugin/TuyaAppSdkPlugin.kt
+++ android/src/main/kotlin/com/example/tuya_app_sdk_plugin/TuyaAppSdkPlugin.kt
@@ -1,7 +1,19 @@
 package com.example.tuya_app_sdk_plugin
 
+import android.app.Application
+import android.content.ContentValues.TAG
+import android.content.Context
 import androidx.annotation.NonNull
-
+import com.thingclips.smart.android.user.api.ILoginCallback
+import com.thingclips.smart.android.user.api.ILogoutCallback
+import com.thingclips.smart.android.user.bean.User
+import com.thingclips.smart.home.sdk.ThingHomeSdk
+import com.thingclips.smart.home.sdk.builder.ActivatorBuilder
+import com.thingclips.smart.sdk.api.IThingActivator
+import com.thingclips.smart.sdk.api.IThingSmartActivatorListener
+import com.thingclips.smart.sdk.bean.DeviceBean
+import com.thingclips.smart.sdk.enums.ActivatorModelEnum
+import io.flutter.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -15,18 +27,110 @@ class TuyaAppSdkPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private var thingActivator: IThingActivator? = null
+  private var applicationContext: Context? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    applicationContext = flutterPluginBinding.applicationContext
+
+    ThingHomeSdk.init(applicationContext as Application)
+
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "tuya_app_sdk_plugin")
     channel.setMethodCallHandler(this)
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "loginWithTicket" -> loginWithTicket(call, result)
+      "pairingDeviceAPMode" -> pairingDeviceAPMode(call, result)
+      "logout" -> logout(call, result)
+      else -> result.notImplemented()
     }
+  }
+
+  private fun loginWithTicket(call: MethodCall, result: Result) {
+    val ticket = call.argument<String>("ticket") ?: ""
+
+    if (ticket.isNotEmpty()) {
+      ThingHomeSdk.getUserInstance().loginWithTicket(ticket, object : ILoginCallback {
+        override fun onSuccess(user: User?) {
+          result.success("SUCCESS")
+        }
+
+        override fun onError(code: String?, error: String?) {
+          result.error(
+            "LOGIN_ERROR",
+            error,
+            null
+          )
+        }
+      })
+    } else {
+      result.error(
+        "ARGUMENTS_ERROR",
+        "Arguments missing.",
+        null
+      )
+    }
+  }
+
+  private fun pairingDeviceAPMode(call: MethodCall, result: Result) {
+    val ssid = call.argument<String>("ssid") ?: ""
+    val password = call.argument<String>("password") ?: ""
+    val token = call.argument<String>("token") ?: ""
+    val timeOut = call.argument<Int?>("time_out")?.toLong() ?: 60
+
+    if (ssid.isNotEmpty() && password.isNotEmpty() && token.isNotEmpty()) {
+      val builder =  ActivatorBuilder()
+        .setContext(applicationContext)
+        .setSsid(ssid)
+        .setPassword(password)
+        .setActivatorModel(ActivatorModelEnum.THING_AP)
+        .setTimeOut(timeOut)
+        .setToken(token)
+        .setListener(object : IThingSmartActivatorListener {
+          override fun onError(errorCode: String, errorMsg: String) {
+            result.error(
+              errorCode,
+              errorMsg,
+              null
+            )
+          }
+
+          override fun onActiveSuccess(devResp: DeviceBean?) {
+            result.success("SUCCESS")
+          }
+
+          override fun onStep(step: String?, data: Any?) {
+            Log.i(TAG, "$step --> $data")
+          }
+        })
+
+      thingActivator = ThingHomeSdk.getActivatorInstance().newActivator(builder)
+      thingActivator?.start()
+    } else {
+      result.error(
+        "ARGUMENTS_ERROR",
+        "Arguments missing.",
+        null
+      )
+    }
+  }
+
+  private fun logout(call: MethodCall, result: Result) {
+    ThingHomeSdk.getUserInstance().logout(object : ILogoutCallback {
+      override fun onSuccess() {
+        result.success("SUCCESS")
+      }
+
+      override fun onError(errorCode: String, errorMsg: String) {
+        result.error(
+          errorCode,
+          errorMsg,
+          null
+        )
+      }
+    })
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
---
+++ example/android/app/src/main/kotlin/com/example/tuya_app_sdk_plugin_example/BaseApplication.kt
@@ -0,0 +1,6 @@
+package com.example.poc_smart_life_flutter_sdk_plugin_example
+
+import android.app.Application
+
+class BaseApplication : Application() { 
+}
